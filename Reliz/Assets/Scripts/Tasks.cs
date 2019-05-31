9edb0f1bc47b78b4ada37c4857a5319400ef663c
danilaklochikhin <danilaklochikhin@yandex.ru>
2019-05-31 17:33:55 +0400

    add scripts for Indicator and Socket

---
+++ Reliz/Assets/Scripts/Tasks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tasks : ScriptableObject
+{
+    public string Description; //
+    public Sprite backgound; // Картинка заднего фона
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Reliz/Assets/Scripts/Tests.cs
+++ Reliz/Assets/Scripts/Tests.cs
@@ -15,4 +15,15 @@ public class Test
 {
     public string Name; // Имя теста
     public string nameScene; // Имя сцены, котоую необходимо будет загрузить
+    public TascList[] tascs; // Задания, необходимые к выполнению
+    public TascList[] securityRegulations; // Правила безопасности, которые необходимо выполнить
+    public Sprite imageBackground; // Визуализация наряд-допуска
+    public int[][] sequenceTasc; // Допустимые последовательности выполнения заданий
+}
+
+public class TascList
+{
+    public string name; // Задание, необходимое к выполнению
+    public bool done; // Флаг выполнения задания
+    public int numberSequence; // Порядковый номер в последовательности выполненых заданий
 }
